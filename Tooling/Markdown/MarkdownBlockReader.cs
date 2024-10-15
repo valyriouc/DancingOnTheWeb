8bfcd5283c7fbfc3b2cd5756ef3acff7c70dccb1
Valacor <valyrious.informatics@gmail.com>
2024-10-15 20:47:22 +0200

    Implement markdown heading parsing

--- Tooling/Markdown/MarkdownBlockReader.cs
+++ Tooling/Markdown/MarkdownBlockReader.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Tooling.Markdown;
 
 public enum NewLine
@@ -41,7 +43,7 @@ public static class MarkdownBlockReader
         {
             span = span
                 .SkipNonSenseCharacters()
-                .ConsumeNewline();
+                .ConsumeNewLine();
             
             if (span[0..3] is "---")
             {
@@ -62,12 +64,12 @@ public static class MarkdownBlockReader
                 .ThrowWhenMalformedMdHeader()
                 .ToString();
             
-            span = span[index..].ConsumeNewline();
+            span = span[index..].ConsumeNewLine();
             
             headers.Add(key.Trim(), value.Trim());
         }
 
-        span = span.ConsumeNewline();
+        span = span.ConsumeNewLine();
 
         return span.Trim();
     }
@@ -79,6 +81,55 @@ public static class MarkdownBlockReader
         {
             return;
         }
+
+        MdBlockType mdType;
+        string content = string.Empty;
+        
+        while (!span.IsEmpty)
+        {
+            span = span.SkipNonSenseCharacters();
+            switch (span[0])
+            {
+                case '#':
+                    span = ReadHeading(span, out mdType, out content);
+                    break;
+                default:
+                    throw new MarkdownException("Unkown markdown block!");
+                    break;
+            }
+            
+            blocks.Add((mdType, content));
+        }
+    }
+
+    private static ReadOnlySpan<char> ReadHeading(
+        ReadOnlySpan<char> span, 
+        out MdBlockType blockType,
+        out string content)
+    {
+        int headingNumber = span.TakeWhile(c => c == '#').Length;
+        if (headingNumber == 0)
+        {
+            throw new MarkdownException("Not a valid markdown heading.");
+        }
+
+        span = span[headingNumber..].ReadLine(out content);
+        switch (headingNumber)
+        {
+            case 1:
+                blockType = MdBlockType.H1;
+                break;
+            case 2:
+                blockType = MdBlockType.H2;
+                break;
+            case 3:
+                blockType = MdBlockType.H3;
+                break;
+            default:
+                throw new MarkdownException("Only headings until level 3 are supported right now.");
+        }
+
+        return span;
     }
 }
 
@@ -106,7 +157,7 @@ internal static class SpanExtensions
         return self;
     }
 
-    public static ReadOnlySpan<char> ConsumeNewline(this ReadOnlySpan<char> self)
+    public static ReadOnlySpan<char> ConsumeNewLine(this ReadOnlySpan<char> self)
     {
         if (self.IsEmpty)
         {
@@ -144,5 +195,53 @@ internal static class SpanExtensions
         }
 
         return self;
+    }
+
+    public static ReadOnlySpan<char> ReadLine(this ReadOnlySpan<char> self, out string line)
+    {
+        line = string.Empty;
+        
+        if (self.IsEmpty)
+        {
+            return self;
+        }
+
+        StringBuilder sb = new();
+        while (true)
+        {
+            if (self[0] == '\r' || self[0] == '\n' || self.IsEmpty)
+            {
+                break;
+            }
+
+            sb.Append(self[0]);
+            self = self[1..];
+        }
+
+        line = sb.ToString();
+
+        return self.ConsumeNewLine();
+    }
+
+    public static string TakeWhile(this ReadOnlySpan<char> self, Func<char, bool> predicate)
+    {
+        if (self.IsEmpty)
+        {
+            return string.Empty;
+        }
+
+        StringBuilder sb = new();
+        while (true)
+        {
+            if (predicate(self[0])) 
+            { 
+                break;    
+            }
+            
+            sb.Append(self[0]);
+            self = self[1..];
+        }
+
+        return sb.ToString();
     }
 }
